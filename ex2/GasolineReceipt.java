01928da45521d887c95a335d78ba5c8d5840470a
addictCoder <addictcodercs@gmail.com>
2017-07-11 22:47:36 -0700

    Exercise 02: More Printing

---
+++ ex2/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt
+{
+	public static void main(String[] args)
+	{
+		System.out.println( "+------------------------+" );
+		System.out.println( "|                        |" );
+		System.out.println( "|      Corner Store      |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|   2017-07-11 10:39PM   |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  Gallons:      10.870  |" );
+		System.out.println( "|  Price/gallon: $ 2.641 |" );
+		System.out.println( "|                        |" );
+		System.out.println( "|  Fuel total:  $ 28.71  |" );
+		System.out.println( "|                        |" );
+		System.out.println( "+------------------------+" );
+	}
+}
